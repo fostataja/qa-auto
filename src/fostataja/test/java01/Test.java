6664016b70884fe0662fd601da904a9973d23e8b
frolova <frolova.n@ipland.com.ua>
2019-09-27 16:34:36 +0300

    refreshing memories

---
+++ src/fostataja/test/java01/Test.java
@@ -0,0 +1,26 @@
+package fostataja.test.java01;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main (String[] args) {
+        int element1, element2;
+        element1 = 5;
+        System.out.println(5);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input smth");
+        element2 = Integer.parseInt(sc.nextLine());
+        System.out.println(element2);
+
+
+        System.out.println(true);
+        System.out.println("test");
+            if (element1 >= element2) {
+                System.out.println("score!");
+            } else {
+              System.out.println("pffff");
+            }
+
+}}
+
