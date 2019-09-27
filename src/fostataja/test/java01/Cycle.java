a2ce2642b62414218a59e7be1efaa915df7b25c3
frolova <frolova.n@ipland.com.ua>
2019-09-27 17:06:53 +0300

    refreshing memories

---
+++ src/fostataja/test/java01/Cycle.java
@@ -0,0 +1,17 @@
+package fostataja.test.java01;
+
+public class Cycle {
+
+    public static void main (String [] args){
+
+        for (int i = 0; i < 20; i++){
+            System.out.println(i);
+        }
+
+        int i = 0;
+        while (i < 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
